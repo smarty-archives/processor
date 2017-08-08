1b8f5cf5c85d928029a523ea3d7e6005a6b89d31
Michael Whatcott <mdwhatcott@gmail.com>
2017-08-08 15:23:27 -0600

    Authentication client setting hostname and scheme (pending auth).

--- authentication_client.go
+++ authentication_client.go
@@ -3,17 +3,22 @@ package processor
 import "net/http"
 
 type AuthenticationClient struct {
+	inner    HTTPClient
 	scheme   string
 	hostname string
 }
 
-func NewAuthenticationClient(scheme string, hostname string) *AuthenticationClient {
+func NewAuthenticationClient(inner HTTPClient, scheme string, hostname string, authId string, authToken string) *AuthenticationClient {
 	return &AuthenticationClient{
+		inner:    inner,
 		scheme:   scheme,
 		hostname: hostname,
 	}
 }
 
-func (this *AuthenticationClient) Do(*http.Request) (*http.Response, error) {
-	panic("implement me")
+func (this *AuthenticationClient) Do(request *http.Request) (*http.Response, error) {
+	request.URL.Scheme = this.scheme
+	request.URL.Host = this.hostname
+	request.Host = this.hostname
+	return this.inner.Do(request)
 }
--- authentication_client_test.go
+++ authentication_client_test.go
@@ -1,36 +1,51 @@
 package processor
 
 import (
+	"net/http/httptest"
 	"testing"
 
-	"github.com/smartystreets/gunit"
-	"net/http/httptest"
+	"net/http"
+
+	"errors"
+
 	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
 )
 
 func TestAuthenticationClient(t *testing.T) {
 	gunit.Run(new(AuthenticationClientFixture), t)
 }
 
-
 type AuthenticationClientFixture struct {
 	*gunit.Fixture
 
-	inner *FakeHTTPClient
+	inner  *FakeHTTPClient
 	client *AuthenticationClient
 }
+
 func (this *AuthenticationClientFixture) Setup() {
 	this.inner = &FakeHTTPClient{}
-	this.client = &AuthenticationClient{}
+	this.client = NewAuthenticationClient(this.inner, "http", "different-company.com", "authid", "authtoken")
 }
 
-func (this *AuthenticationClientFixture) TestHostnameAndScheme() {
-	this.client.WithHostname("us-street.api.smartystreets.com")
+func (this *AuthenticationClientFixture) TestProvideInformationAddedBeforeRequestIsSent() {
 	request := httptest.NewRequest("GET", "/path", nil)
 
 	this.client.Do(request)
 
-	this.So(this.inner.request.Host, should.Equal, "us-street.api.smartystreets.com")
-	this.So(this.inner.request.URL.Scheme, should.Equal, "https")
+	this.So(this.inner.request.URL.Scheme, should.Equal, "http")
+	this.So(this.inner.request.Host, should.Equal, "different-company.com")
+	this.So(this.inner.request.URL.Host, should.Equal, "different-company.com")
+	this.So(this.inner.request.URL.Query().Get("auth-id"), should.Equal, "authid")
+	this.So(this.inner.request.URL.Query().Get("auth-token"), should.Equal, "authtoken")
 }
 
+func (this *AuthenticationClientFixture) TestResponseAndErrorFromInnerClientReturned() {
+	this.inner.response = &http.Response{StatusCode: http.StatusTeapot}
+	this.inner.err = errors.New("HTTP Error")
+	request := httptest.NewRequest("GET", "/path", nil)
+	response, err := this.client.Do(request)
+
+	this.So(response.StatusCode, should.Equal, http.StatusTeapot)
+	this.So(err.Error(), should.Equal, "HTTP Error")
+}
