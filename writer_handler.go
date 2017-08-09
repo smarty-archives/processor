13fca750b4d7e17b6a52ebbba3ff4145ca2690a6
Michael Whatcott <mdwhatcott@gmail.com>
2017-08-09 10:18:24 -0600

    Implemented the writer.

--- writer_handler.go
+++ writer_handler.go
@@ -12,15 +12,34 @@ type WriterHandler struct {
 }
 
 func NewWriterHandler(input chan *Envelope, output io.WriteCloser) *WriterHandler {
-	return &WriterHandler{
+	this := &WriterHandler{
 		input:  input,
 		closer: output,
 		writer: csv.NewWriter(output),
 	}
+	this.writeValues("Status", "DeliveryLine1", "City", "State", "ZIPCode")
+	return this
 }
 
 func (this *WriterHandler) Handle() {
-	this.writer.Write([]string{"Status", "DeliveryLine1", "City", "State", "ZIPCode"})
+	for envelope := range this.input {
+		this.writeAddressOutput(envelope.Output)
+	}
+
 	this.writer.Flush()
 	this.closer.Close()
+}
+
+func (this *WriterHandler) writeAddressOutput(output AddressOutput) {
+	this.writeValues(
+		output.Status,
+		output.DeliveryLine1,
+		output.LastLine,
+		output.City,
+		output.State,
+		output.ZIPCode)
+}
+
+func (this *WriterHandler) writeValues(values ...string) {
+	this.writer.Write(values)
 }
--- writer_handler_test.go
+++ writer_handler_test.go
@@ -3,6 +3,8 @@ package processor
 import (
 	"bytes"
 	"encoding/csv"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/smartystreets/assertions/should"
@@ -29,23 +31,51 @@ func (this *WriterHandlerFixture) Setup() {
 }
 
 func (this *WriterHandlerFixture) TestHeaderWritten() {
+	close(this.input)
 	this.handler.Handle()
 
 	this.So(this.buffer.String(), should.Equal, "Status,DeliveryLine1,City,State,ZIPCode\n")
 }
 
+func (this *WriterHandlerFixture) TestAllEnvelopesWritten() {
+	this.sendEnvelopes(2)
+	this.handler.Handle()
+
+	if lines := this.outputLines(); this.So(lines, should.HaveLength, 3) {
+		this.So(lines[1], should.Equal, "A1,B1,C1,D1,E1,F1")
+		this.So(lines[2], should.Equal, "A2,B2,C2,D2,E2,F2")
+	}
+}
+
 func (this *WriterHandlerFixture) TestOutputClosed() {
+	close(this.input)
 	this.handler.Handle()
 
 	this.So(this.buffer.closed, should.Equal, 1)
 }
 
-func (this *WriterHandlerFixture) TestEnvelopeWritten() {
-	this.input <- &Envelope{AddressOutput{
-		Status: "Status",
-		
-	}}
+func (this *WriterHandlerFixture) sendEnvelopes(count int) {
+	for x := 1; x < count+1; x++ {
+		this.input <- &Envelope{
+			Output: createOutput(strconv.Itoa(x)),
+		}
+	}
 	close(this.input)
+}
+func createOutput(index string) AddressOutput {
+	return AddressOutput{
+		Status:        "A" + index,
+		DeliveryLine1: "B" + index,
+		LastLine:      "C" + index,
+		City:          "D" + index,
+		State:         "E" + index,
+		ZIPCode:       "F" + index,
+	}
+}
+
+func (this *WriterHandlerFixture) outputLines() []string {
+	outputFile := strings.TrimSpace(this.buffer.String())
+	return strings.Split(outputFile, "\n")
 }
 
 //////////////////////////////////////////////////////////
